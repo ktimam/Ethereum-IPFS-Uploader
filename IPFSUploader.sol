9de1842495b62b9e574c23e907c913eca5c7d7f9
ktimam <ktimam@users.noreply.github.com>
2019-09-25 16:23:44 +0200

    Initial Import

---
+++ IPFSUploader.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.5.11;
+
+import "./provableAPI.sol";
+
+contract IPFSUploader is usingProvable  {
+
+    address public owner;
+    string public IPFSHash;
+    string public provableComputationHash;
+    mapping(bytes32=>bool) validIds;
+    event LogConstructorInitiated(string nextStep);
+    event LogNewProvableQuery(string description);
+    event LogIPFSHashReceived(string hash);
+    
+    modifier ownerOnly() {
+      if (msg.sender == owner) _;
+    }
+
+    
+    constructor () public payable  {
+        owner = msg.sender;
+        
+        provable_setCustomGasPrice(4000000000);
+        provable_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        emit LogConstructorInitiated("Constructor was initiated. Call 'uploadToIPFS(text)' to send the Provable Query.");
+    }
+    
+    function setProvableComputationHash(string memory _provableComputationHash) public ownerOnly
+    {
+        provableComputationHash = _provableComputationHash;
+    }
+
+    function __callback(bytes32 _myid, string memory _result, bytes memory _proof) public {
+        if (!validIds[_myid]) revert();
+        if (msg.sender != provable_cbAddress()) revert();
+        IPFSHash = _result;
+        emit LogIPFSHashReceived(_result);
+        delete validIds[_myid];
+    }
+
+    function uploadToIPFS(string memory _text) public payable {
+        if (provable_getPrice("URL") > address(this).balance) {
+          emit LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+          emit LogNewProvableQuery("Provable query was sent, standing by for the answer..");
+          bytes32 queryId =
+            provable_query("computation", ["QmSJ7RL8JFy6p2br5874SQBMe9qZiDZHayC9N6bx4GzMem", _text, ""]);
+          validIds[queryId] = true;
+        }
+    }
+}
